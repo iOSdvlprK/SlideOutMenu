c7789c1daf347e9980d04ba64a6f6edca5f2407d
iOS developer K <iOSdvlprK@gmail.com>
2022-12-23 03:06:14 +0900

    refactoring onto search container view

--- SlideOutMenu/Controllers/ChatroomMenuContainerController.swift
+++ SlideOutMenu/Controllers/ChatroomMenuContainerController.swift
@@ -10,10 +10,7 @@ import UIKit
 class ChatroomMenuContainerController: UIViewController {
     
     let chatroomsMenuController = ChatroomsMenuController()
-    
-    let searchContainer = UIView()
-    let rocketImageView = UIImageView(image: #imageLiteral(resourceName: "rocket"))
-    let searchBar = UISearchBar()
+    let searchContainer = SearchContainerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,30 +23,44 @@ class ChatroomMenuContainerController: UIViewController {
         view.addSubview(searchContainer)
         searchContainer.backgroundColor = #colorLiteral(red: 0.2549019608, green: 0.1843137255, blue: 0.2470588235, alpha: 1)
         
-        rocketImageView.contentMode = .scaleAspectFit
-        rocketImageView.layer.cornerRadius = 3
-        rocketImageView.clipsToBounds = true
-        view.addSubview(rocketImageView)
-        rocketImageView.anchor(top: nil, leading: view.leadingAnchor, bottom: searchContainer.bottomAnchor, trailing: nil, padding: .init(top: 0, left: 8, bottom: 12, right: 0), size: .init(width: 40, height: 40))
-        
-        
-        searchContainer.addSubview(searchBar)
-        searchBar.searchBarStyle = .minimal
-//        searchBar.placeholder = "Enter some filer"    // doesn't work
-        searchBar.searchTextField.textColor = .white
-        searchBar.searchTextField.leftView?.tintColor = .gray   // magnify color
-//        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).textColor = .white
-        searchBar.anchor(top: nil, leading: rocketImageView.trailingAnchor, bottom: searchContainer.bottomAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: 4, right: 0))
-        
         searchContainer.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor)
         searchContainer.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 64).isActive = true
         
         chatroomsView.anchor(top: searchContainer.bottomAnchor, leading: view.safeAreaLayoutGuide.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        searchBar.searchTextField.attributedPlaceholder = NSAttributedString(string: "Enter some filter", attributes: [.foregroundColor: UIColor.gray])
-    }
 
+}
+
+class SearchContainerView: UIView {
+    let searchBar: UISearchBar = {
+       let sb = UISearchBar()
+        sb.searchBarStyle = .minimal
+        sb.searchTextField.textColor = .white
+        sb.searchTextField.leftView?.tintColor = .gray   // magnify color
+        sb.searchTextField.attributedPlaceholder = NSAttributedString(string: "Enter some filter", attributes: [.foregroundColor: UIColor.gray])
+        return sb
+    }()
+    
+    let rocketImageView: UIImageView = {
+        let iv = UIImageView(image: #imageLiteral(resourceName: "rocket"))
+        iv.contentMode = .scaleAspectFit
+        iv.layer.cornerRadius = 3
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(searchBar)
+        addSubview(rocketImageView)
+        
+        rocketImageView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: nil, padding: .init(top: 0, left: 8, bottom: 12, right: 0), size: .init(width: 40, height: 40))
+        searchBar.anchor(top: nil, leading: rocketImageView.trailingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, padding: .init(top: 0, left: 0, bottom: 4, right: 0))
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
