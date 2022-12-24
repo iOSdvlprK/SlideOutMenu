c3f6b6e0f758301f57c55034e07b6f762372a4e8
iOS developer K <iOSdvlprK@gmail.com>
2022-12-25 01:29:12 +0900

    circular navigation bar button

--- SlideOutMenu/Controllers/HomeController.swift
+++ SlideOutMenu/Controllers/HomeController.swift
@@ -37,8 +37,33 @@ class HomeController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     fileprivate func setupNavigationItems() {
         navigationItem.title = "Home"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        setupCircularNavigationButton()
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    fileprivate func setupCircularNavigationButton() {
+        let image = #imageLiteral(resourceName: "girl_profile").withRenderingMode(.alwaysOriginal)
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: self, action: #selector(handleOpen))  // 1) this doesn't work
+        let customView = UIButton(type: .system)
+//        customView.backgroundColor = .orange
+        customView.addTarget(self, action: #selector(handleOpen), for: .touchUpInside)
+//        customView.imageView?.image = image // 2) this doesn't work
+        customView.setImage(image, for: .normal)
+        customView.imageView?.contentMode = .scaleAspectFit
+//        customView.frame = CGRect(x: 0, y: 0, width: 40, height: 40) // 3) ditto
+//        customView.layer.cornerRadius = 20  // 4) It doesn’t work, either.
+        
+        // ✅ custom view uses auto layout to put itself in the nav bar
+        customView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        customView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        customView.layer.cornerRadius = 20
+        customView.clipsToBounds = true
+        
+        let barButtonItem = UIBarButtonItem(customView: customView)
+        navigationItem.leftBarButtonItem = barButtonItem
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
